20bf2147f182b9f2485c8194148290f85d398b80
sequelster <42946708+sequelster@users.noreply.github.com>
2018-09-13 17:57:06 -0400

    this file is useless
    Good thing that username/pass is for a local garbage database.
    HOO BOY.

--- query.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-
-mysql_engine = create_engine('mysql://root:arglblargl@localhost/test_skaters', isolation_level="AUTOCOMMIT")
-
-connection = mysql_engine.connect()
-results = connection.execute("SELECT * FROM members ORDER BY id ASC")
-for row in results:
-    print (row)
-connection.close()
