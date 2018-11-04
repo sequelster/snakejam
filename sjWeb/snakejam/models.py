db0586281daf3d72160d739eb1a3d495b87ccda4
sequelster <tlrhodes@buffalo.edu>
2018-11-03 20:59:56 -0400

    Fix choice syntax, add simple view for members.

--- sjWeb/snakejam/models.py
+++ sjWeb/snakejam/models.py
@@ -7,6 +7,8 @@ class Teams(models.Model):
 
     class Meta:
         db_table = "teams"
+    def __str__(self):
+        return self.name
 
 class Members (models.Model):
     full_name = models.CharField(unique=True, max_length=150, db_index=True)
@@ -16,16 +18,19 @@ class Members (models.Model):
     class Meta:
         db_table = "members"
 
+    def __str__(self):
+        return self.full_name
+
 class TeamMember(models.Model):
     ROLES = (
-        ('PIVOT', 'pivot'),
-        ('JAMMER', 'jammer'),
-        ('BLOCKER', 'blocker'),
-        ('COACH', 'coach'),
-        ('CAPTAIN', 'captain'))
-    full_name = models.ForeignKey(Members, to_field='full_name', db_column='full_name', related_name='member_name', on_delete=models.CASCADE)
-    team = models.ForeignKey(Teams, to_field='name', related_name='membership', on_delete=models.CASCADE)
-    role = MultiSelectField(max_length=6, choices=ROLES)
+        (1, 'pivot'),
+        (2, 'jammer'),
+        (3, 'blocker'),
+        (4, 'coach'),
+        (5, 'captain'))
+    full_name = models.ForeignKey('Members', to_field='full_name', db_column='full_name', related_name='member_name', on_delete=models.CASCADE)
+    team = models.ForeignKey('Teams', to_field='name', related_name='membership', on_delete=models.CASCADE)
+    role = MultiSelectField(max_length=5, max_choices=5, choices=ROLES)
     class Meta:
         db_table = 'team_members'
         ordering = ['team', 'full_name']
--- sjWeb/snakejam/views.py
+++ sjWeb/snakejam/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Members
 # Create your views here.
 
 def index(request):
     return HttpResponse("hellooooo. Welcome to the index.")
+
+def member_list(request):
+    members = Members.objects.all()
+    return render(request, 'members/members.html', {'members': members})
